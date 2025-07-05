11197890dfe8943de66e0c54abf9458e4737c0c8
Tsinat <tsinatyilma16@gmail.com>
2025-07-05 15:11:12 +0300

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+  <div className="h-screen relative">
+      <p className="border-2 text-2xl font-bold text-red-700 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+        Dashboard Page
+      </p>
+    </div>
+)}
